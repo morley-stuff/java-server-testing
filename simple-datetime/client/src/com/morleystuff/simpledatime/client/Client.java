d25807b167e746dcb45d9fa0a5ee055b6ca27b66
morley-stuff <morleyj.mail@gmail.com>
2019-04-06 13:00:44 +1300

    Implemented functionality for client and singlethreaded server to get current datetime

--- simple-datetime/client/src/com/morleystuff/simpledatime/client/Client.java
+++ simple-datetime/client/src/com/morleystuff/simpledatime/client/Client.java
@@ -1,9 +1,20 @@
 package com.morleystuff.simpledatime.client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        if (args.length != 1) {
+            System.out.println("Pass the server ip as the only commandline argument");
+            return;
+        }
+        Socket socket = new Socket(args[0], 59090);
+        Scanner in = new Scanner(socket.getInputStream());
+        System.out.println("Server Response: " + in.nextLine());
     }
 
 }
