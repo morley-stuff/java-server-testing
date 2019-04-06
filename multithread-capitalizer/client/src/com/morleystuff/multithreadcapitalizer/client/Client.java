c82fb19224502a27d9e2f52cd4194b80f4b6316c
morley-stuff <morleyj.mail@gmail.com>
2019-04-06 14:13:28 +1300

    Added functionality for multithreaded capitalizer server and client

--- multithread-capitalizer/client/src/com/morleystuff/multithreadcapitalizer/client/Client.java
+++ multithread-capitalizer/client/src/com/morleystuff/multithreadcapitalizer/client/Client.java
@@ -1,7 +1,25 @@
 package com.morleystuff.multithreadcapitalizer.client;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Pass the server ip as the only commandline arg");
+            return;
+        }
+        try (Socket socket = new Socket(args[0],59091)) {
+            System.out.println("Enter lines of text the Ctrl+D or Ctrl+C to exit");
+            Scanner scanner = new Scanner(System.in);
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while(scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+                System.out.println(in.nextLine());
+            }
+        }
     }
 }
--- multithread-capitalizer/server/src/com/morleystuff/multithreadcapitalizer/server/Server.java
+++ multithread-capitalizer/server/src/com/morleystuff/multithreadcapitalizer/server/Server.java
@@ -1,7 +1,51 @@
 package com.morleystuff.multithreadcapitalizer.server;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket listener = new ServerSocket(59091)) {
+            ExecutorService pool = Executors.newFixedThreadPool(20);
+            System.out.println("Server is running...");
+            while (true) {
+                pool.execute(new Capitalizer(listener.accept()));
+            }
+        }
     }
+
+    private static class Capitalizer implements Runnable {
+        private Socket socket;
+
+        Capitalizer(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Connected: " + socket);
+            try {
+                Scanner in = new Scanner(socket.getInputStream());
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                while (in.hasNextLine()) {
+                    out.println(in.nextLine().toUpperCase());
+                }
+            } catch (IOException e) {
+                System.out.println("Error: " + socket);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+
+                }
+                System.out.println("Closed: " + socket);
+            }
+        }
+    }
+
 }
