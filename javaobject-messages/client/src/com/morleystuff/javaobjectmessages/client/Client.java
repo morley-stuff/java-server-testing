b9636c39ea9cbfb1342abcaa27bf3e61d2a57d54
morley-stuff <morleyj.mail@gmail.com>
2019-04-07 18:05:15 +1200

    Added functionality for java object messaging server and client

--- javaobject-messages/client/src/com/morleystuff/javaobjectmessages/client/Client.java
+++ javaobject-messages/client/src/com/morleystuff/javaobjectmessages/client/Client.java
@@ -1,9 +1,32 @@
 package com.morleystuff.javaobjectmessages.client;
 
+import com.morleystuff.javaobjectmessages.core.Message;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        if (args.length != 1) {
+            System.out.println("Pass the server ip as the only commandline arg");
+            return;
+        }
+        try (Socket socket = new Socket(args[0], 59092)) {
+            System.out.println("Connected on: " + socket);
+            Scanner userInput = new Scanner(System.in);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Setup streams successfully");
+            while (userInput.hasNextLine()) {
+                Message myMessage = new Message(userInput.nextLine());
+                objectOutputStream.writeObject(myMessage);
+                Message receivedMessage = (Message) objectInputStream.readObject();
+                System.out.println(receivedMessage.getMessage());
+            }
+        }
     }
 
 }
--- javaobject-messages/server/src/com/morleystuff/javaobjectmessages/server/Server.java
+++ javaobject-messages/server/src/com/morleystuff/javaobjectmessages/server/Server.java
@@ -1,9 +1,37 @@
 package com.morleystuff.javaobjectmessages.server;
 
+import com.morleystuff.javaobjectmessages.core.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        try (ServerSocket listener = new ServerSocket(59092)) {
+            System.out.println("Listening for new connections...");
+            while (true) {
+                try (Socket socket = listener.accept()) {
+                    System.out.println("Connected on: " + socket);
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                    ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                    while (true) {
+                        Message receivedMessage = (Message) objectInputStream.readObject();
+                        if (receivedMessage == null) {
+                            break;
+                        }
+                        System.out.println("Received message: " + receivedMessage.getMessage());
+                        Message myMessage = new Message(receivedMessage.getMessage().toUpperCase());
+                        objectOutputStream.writeObject(myMessage);
+                    }
+                }
+                System.out.println("Connection closed");
+            }
+        }
     }
 
 }
